b0a36dcf86acfbc21aa156bdb6b41558a49b0353
henry <henry.aspeti@gmail.com>
2023-09-27 02:39:13 -0400

    Editar perfil y contraseña

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -55,6 +55,31 @@
             </ul>
           </li>  
           <li class="nav-item">
+                    <a href="#" class="nav-link">                           
+                      <i class="nav-icon fas fa-cog"></i>           
+                      <p>
+                        Profile
+                        <i class="fas fa-angle-left right"></i>
+                      </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                        <a href="<?php echo base_url()?>usuarios/viewprofile/<?php echo $this->session->userdata('id_usuario'); ?> " class="nav-link">
+                          <i class="fas fa-user nav-icon"></i>
+                          <p>Perfil</p>
+                        </a>
+                      </li>   
+                    </ul>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                      <a href="<?php echo base_url('usuarios/password/'.$this->session->userdata('id_usuario')); ?>" class="nav-link">
+                          <i class="fas fa-user nav-icon"></i>
+                          <p>Password</p>
+                        </a>
+                      </li>   
+                    </ul>           
+                  </li>
+          <li class="nav-item">
                       <a href="<?php echo base_url();?>#" class="nav-link">
                       <i class="fas fa-money-bill nav-icon"></i>
                       <p>Ventas</p>
