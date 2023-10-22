add94c2e2ea4cae3aec445c6c8658e0ae273296b
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-22 19:19:00 -0400

    Alter schema  almohadas

--- scripts/20231003_001_alterTable_add_Img_Colum_In_Product_MD.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `almohadas_db`.`producto` 
-ADD COLUMN `img` VARCHAR(45) NULL AFTER `precio`;
-
-ALTER TABLE `almohadas_db`.`producto` 
-ADD COLUMN `codigo` VARCHAR(45) NULL AFTER `precio`;
