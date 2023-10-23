95b777a26f237fa217fffd5b09c5ea36ee792c32
henry <henry.aspeti@gmail.com>
2023-10-23 17:29:23 -0400

    update inserts

--- scripts/inserts.sql
+++ scripts/inserts.sql
@@ -10,3 +10,19 @@ insert into usuario(id_usuario,fecha_creacion,eliminado,email,password,nombre,ap
 
 
 insert into comprobante (id_comprobante,nombre,cantidad,serie)values(1,'recibo',1,'001')
+
+insert into categoria(nombre, descripcion, eliminado) value
+("Comercial", "Almohadas comerciales",0), 
+("Empresarial", "Almohadas empresarial",0)
+
+
+insert into categoria(id_categoria,nombre, descripcion, eliminado) value
+(1,"Comercial", "Almohadas comerciales",0), 
+(2,"Empresarial", "Almohadas empresarial",0);
+
+
+INSERT INTO producto (codigo, fecha_creacion, eliminado, nombre, descripcion, precio, id_categoria)
+VALUES
+('ALM001', NOW(), 0, 'Almohada Suave', 'Almohada suave y cómoda para un sueño tranquilo', 25.00, 1),
+('ALM002', NOW(), 0, 'Almohada Firme', 'Almohada firme para un soporte adecuado del cuello', 30.00, 2),
+('ALM003', NOW(), 0, 'Almohada Ortopédica', 'Almohada ortopédica para un mejor soporte de la espalda', 35.00, 1);
