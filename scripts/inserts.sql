8d716ad817399df93f454c80c2453d8f5954b188
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-26 19:08:37 -0400

    change insert

--- scripts/inserts.sql
+++ scripts/inserts.sql
@@ -4,12 +4,12 @@ insert into rol (id_rol,nombre, descripcion, eliminado)values
 
 insert into usuario(id_usuario,fecha_creacion,eliminado,email,password,nombre,apellido,id_rol)values
 (1,CURDATE(),0,"admin@gmail.com",MD5("admin"),'Admin','Apellido',1),
-(2,CURDATE(),0,"usuario@gmail.com",MD5("usuario"),'usuario','Vargas',2);
-(3,CURDATE(),0,"fabiola@gmail.com",MD5("fabiola"),'Fabiola','Perez',2);
+(2,CURDATE(),0,"usuario@gmail.com",MD5("usuario"),'usuario','Vargas',2),
+(3,CURDATE(),0,"fabiola@gmail.com",MD5("fabiola"),'Fabiola','Perez',2),
 (4,CURDATE(),0,"mario@gmail.com",MD5("mario"),'Mario','Lopez',2);
 
 
-insert into comprobante (id_comprobante,nombre,cantidad,serie)values(1,'recibo',1,'001')
+insert into comprobante (id_comprobante,nombre,cantidad,serie)values(1,'recibo',1,'001');
 
 
 insert into categoria(id_categoria,nombre, descripcion, eliminado) value
